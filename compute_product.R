9f1ff6b582048ea20b2a8f9bb6c7ad81046c3683
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-08-08 19:01:42 -0400

    Update compute_product.R

--- compute_product.R
+++ compute_product.R
@@ -1,18 +1,16 @@
-compute_product <- function(faasr) {
+compute_product <- function(folder, input1, input2, output) {
 
   # retrieve the arguments for this function
   # This function takes two input file names and one output file name as arguments
   # these become available as args$input1, args$input2, and args$output
   #
-  args <- faasr_get_user_function_args(faasr)
-  
   # Download the input files from S3 bucket
   # The bucket is configured in the JSON payload as My_S3_Bucket
   # In this demo code, all inputs/outputs are in the same S3 folder, which is also configured by the user
   # The downloaded files are stored in a "local" folder under names input1.csv and input2.csf
   #
-  faasr_get_file(faasr, "My_S3_Bucket", args$folder, args$input1, "local", "input1.csv")
-  faasr_get_file(faasr, "My_S3_Bucket", args$folder, args$input2, "local", "input2.csv")
+  faasr_get_file(faasr, "My_S3_Bucket", folder, input1, "local", "input1.csv")
+  faasr_get_file(faasr, "My_S3_Bucket", folder, input2, "local", "input2.csv")
   
   # This demo function computes output <- input1 * input2 and stores the output back into S3
   # First, read the local inputs, compute the sum, and store the output locally
@@ -24,10 +22,10 @@ compute_product <- function(faasr) {
 
   # Now, upload the output file to the S3 bucket
   #
-  faasr_put_file(faasr, "My_S3_Bucket", "local", "output.csv", args$folder, args$output)
+  faasr_put_file(faasr, "My_S3_Bucket", "local", "output.csv", folder, output)
 
   # Print a log message
   # 
-  log_msg <- paste0('Function compute_product finished; output written to ', args$folder, '/', args$output, ' in My_S3_Bucket defined in the JSON configuration')
+  log_msg <- paste0('Function compute_product finished; output written to ', folder, '/', output, ' in My_S3_Bucket defined in the JSON configuration')
   faasr_log(faasr, log_msg)
 }	
