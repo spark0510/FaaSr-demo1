bea07942fb381530fab5472af624f94e844e976e
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-09-08 15:09:26 -0400

    Update combine_data.R

--- combine_data.R
+++ combine_data.R
@@ -11,8 +11,8 @@ combine_data <- function(folder, input1, input2, output) {
   # In this demo code, all inputs/outputs are in the same S3 folder, which is also configured by the user
   # The downloaded files are stored in a "local" folder under names input1.csv and input2.csf
   #
-  faasr_get_file(faasr, "My_S3_Bucket", folder, input1, "local", "input1.csv")
-  faasr_get_file(faasr, "My_S3_Bucket", folder, input2, "local", "input2.csv")
+  faasr_get_file("My_S3_Bucket", folder, input1, "local", "input1.csv")
+  faasr_get_file("My_S3_Bucket", folder, input2, "local", "input2.csv")
   
   # This demo function computes output <- input1 + input2 and stores the output back into S3
   # First, read the local inputs, compute the sum, and store the output locally
@@ -24,10 +24,10 @@ combine_data <- function(folder, input1, input2, output) {
 
   # Now, upload the output file to the S3 bucket
   #
-  faasr_put_file(faasr, "My_S3_Bucket", "local", "output.csv", folder, output)
+  faasr_put_file("My_S3_Bucket", "local", "output.csv", folder, output)
 
   # Print a log message
   # 
   log_msg <- paste0('Function combine_data finished; output written to ', folder, '/', output, ' in My_S3_Bucket defined in the JSON configuration')
-  faasr_log(faasr, log_msg)
+  faasr_log(log_msg)
 }	
